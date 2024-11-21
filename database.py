11d8b4e41b4dc3dde4f3330d80de63512b033652
uwangjar <ubonsri9w@gmail.com>
2024-11-20 18:48:55 -0800

    Initial commit

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE='postgresql://postgres:admin@localhost:5432/QuizApplicationY'
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
